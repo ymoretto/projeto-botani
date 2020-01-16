862f11989b099cd3cd9c97c0318f55e371d499d0
Yolanda <ymoretto@gmail.com>
2020-01-15 23:57:55 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require_once("header.php"); ?>
+<!-- início da home -->
+
+<div class="jumbotron jumbotron-bgimage" >
+  <div class="container">
+    <h1 class="display-4">Transforme seu bairro</h1>
+    <p class="lead">Encontre os melhores serviços, comércios e eventos de sua região</p>
+  </div>
+</div>
+
+
+
+
+<!-- final da home -->
+<?php require_once("footer.php") ?>
