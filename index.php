3223e285099d41e5ebeadf156623b107c10cf2ae
Yolanda <ymoretto@gmail.com>
2020-01-17 17:15:13 -0300

    header desktop finalizado

--- index.php
+++ index.php
@@ -1,15 +1,63 @@
-<?php require_once("header.php"); ?>
-<!-- início da home -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!-- Fim do Boostrap -->
+  <link rel="shortcut icon" href="#" sizes="32x32" type="image/png">
+  <link rel="stylesheet" href="css/geral-style.css">
+  <link rel="stylesheet" href="css/header-style.css">
 
-<div class="jumbotron jumbotron-bgimage" >
-  <div class="container">
-    <h1 class="display-4">Transforme seu bairro</h1>
-    <p class="lead">Encontre os melhores serviços, comércios e eventos de sua região</p>
-  </div>
-</div>
+
+  <title>Botani</title>
+</head>
+
+<header class="container">
+  <nav class="navbar navbar-expand-lg navbar-light row d-flex justify-content-center">
+
+      <div class="" id="navbarNav">
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#sobre">Sobre Nós</a>
+              </li>
+          </ul>
+      </div>
+
+      <div class="navbar-logo">
+        <a class="navbar-brand" href="index.php">
+            <img src="images/botani-logo-folha.png" alt="Botani" class="logo-header">
+        </a>
+      </div>
+
+      <div class="" id="navbarNav">
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="#login">Login</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#contato">Contato</a>
+              </li>
+          </ul>
+      </div>
+      
+  </nav>
+  </header>
 
 
 
+<body>
 
-<!-- final da home -->
-<?php require_once("footer.php") ?>
+<!-- Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<!-- Fim do Bootstrap -->
+
+</body>
+</html>
