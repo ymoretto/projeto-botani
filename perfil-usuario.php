349f38645566aea5b0bcf2939857dae3c519fbe5
Yolanda <ymoretto@gmail.com>
2020-01-29 21:45:06 -0300

    primeira modificacoes perfil usuario

--- perfil-usuario.php
+++ perfil-usuario.php
@@ -1,5 +1,18 @@
 <?php require_once("menu-usuario.php") ?>
 
+<section id="info-usuario">
+    <div class="container">
+    
+        <div class="row">
+            <div class="col">
+                <img src="images/perfil-botania-planticia.jpg" alt="">
+            </div>
 
+            <div class="col">
+            </div>
+        </div>
+
+    </div>
+</section>
 
 <?php require_once("footer.php") ?>
