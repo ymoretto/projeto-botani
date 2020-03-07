e3e8a1ba1b635b546475480735ecfc057b8a43b2
Heloísa Medeiros <heloisam.silva@hotmail.com>
2020-03-07 12:47:24 -0300

    css desktop da pagina de login pronto

--- login.php
+++ login.php
@@ -6,48 +6,52 @@
 
         <!-- Lado Esquerdo da Tela [Login] -->
         <div class="col col-md-6">
-            <div class="left-side d-flex justify-content-center">
-                       
-                        <form method="get" class="formularioLogin">
-                            <div class="form-group">
-                                <h2 class="tituloLogin">Sign in</h2>
-                            </div>
-                            <div class="form-group">
-                                <input type="email" name="email" id="email" placeholder="name@email.com"
-                                    class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <input type="password" name="passwordLogin" id="passwordLogin" class="form-control" placeholder="password">
-                            </div>
-                            <div class="form-group">
-                                <button class="btn btn-primary btnLogin">></button>
-                            </div>
-                        </form>
-                
+            <div class="halfpage d-flex justify-content-center align-items-center">
+                <div class="formplaceholder">
+                    <form method="get" class="formularioLogin">
+                        <div class="form-group">
+                            <h2 class="tituloLogin">Login</h2>
+                        </div>
+                        <div class="form-group">
+                            <input type="email" name="email" id="email" placeholder="name@email.com"
+                                class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="passwordLogin" id="passwordLogin" class="form-control"
+                                placeholder="password">
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-primary btnLogin">Entrar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-        
+
         <!-- Lado Direito da Tela [Cadastro] -->
         <div class="col col-md-6">
-            <div class="right-side d-flex justify-content-center">
-                <form method="post" class="formularioLogin">
-                    <div class="form-group">
-                        <h2 class="tituloLogin">Cadastre-se</h2>
-                    </div>
-                    <div class="form-group">
-                        <input type="email" name="email" id="email" placeholder="someemail@email.com"
-                            class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="passwordLogin" id="passwordLogin" class="form-control" placeholder="password">
-                    </div>
-                    <div class="form-group">
-                        <a href="cadastro.php"><button class="btn btn-primary btnLogin">></button></a>
-                    </div>
-                </form>
+            <div class="halfpage d-flex justify-content-center align-items-center">
+                <div class="formplaceholder">
+                    <form method="post" class="formularioLogin">
+                        <div class="form-group">
+                            <h2 class="tituloLogin">Cadastre-se</h2>
+                        </div>
+                        <div class="form-group">
+                            <input type="email" name="email" id="email" placeholder="someemail@email.com"
+                                class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="passwordLogin" id="passwordLogin" class="form-control"
+                                placeholder="password">
+                        </div>
+                        <div class="form-group">
+                            <a href="cadastro.php"><button class="btn btn-primary btnLogin">Entrar</button></a>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-        
+
     </div>
 </section>
 <!-- Fim da Section de Login -->
