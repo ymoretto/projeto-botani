9b66138f416b508ed9eb49861fe6be1110f85373
Heloísa Medeiros <heloisam.silva@hotmail.com>
2020-03-07 15:14:48 -0300

    setup timelinePlanta

--- timeline-planta.php
+++ timeline-planta.php
@@ -1,5 +1,178 @@
 <?php require_once("menu-usuario.php") ?>
 
+<div id="carouselPlantas" class="carousel slide" data-ride="false">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="container">
 
+        <h2 class="mt-4 mb-3">
+          <small>Nome da Planta</small>
+        </h2>
+
+        <!-- Área da Imagem e descrição -->
+        <div class="row">
+
+          <div class="col-md-8 text-center">
+            <img class="img-fluid image-border plant-image" src="images/jiboia.jpeg" alt="">
+          </div>
+
+          <div class="col-md-4">
+            <h3 class="my-3">Nome da Planta</h3>
+            <p>Uma descrição da planta aqui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+              euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum
+              nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+            <ul>
+              <li>Espécie</li>
+              <li>Desde dd/mm/aa</li>
+              <li>4 flores</li>
+              <li>Algo do tipo</li>
+            </ul>
+          </div>
+
+        </div>
+        <!-- Fim da área da imagem  -->
+
+        <!-- Linha do tempo da planta -->
+        <h3 class="my-4">Linha do Tempo da Planta</h3>
+
+        <div class="row">
+          <div class="container">
+            <div class="timeline">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+            </div>
+          </div>
+        </div>
+        <!-- Fim da linha do tempo -->
+
+      </div> <!-- div que fecha o container -->
+    </div>
+    <div class="carousel-item">
+      <div class="container">
+
+        <h2 class="mt-4 mb-3">
+          <small>Nome da Planta</small>
+        </h2>
+
+        <!-- Área da Imagem e descrição -->
+        <div class="row">
+
+          <div class="col-md-8 text-center">
+            <img class="img-fluid image-border plant-image" src="images/jiboia.jpeg" alt="">
+          </div>
+
+          <div class="col-md-4">
+            <h3 class="my-3">Nome da Planta</h3>
+            <p>Uma descrição da planta aqui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+              euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum
+              nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+            <ul>
+              <li>Espécie</li>
+              <li>Desde dd/mm/aa</li>
+              <li>4 flores</li>
+              <li>Algo do tipo</li>
+            </ul>
+          </div>
+
+        </div>
+        <!-- Fim da área da imagem  -->
+
+        <!-- Linha do tempo da planta -->
+        <h3 class="my-4">Linha do Tempo da Planta</h3>
+
+        <div class="row">
+          <div class="container">
+            <div class="timeline">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+            </div>
+          </div>
+        </div>
+        <!-- Fim da linha do tempo -->
+
+      </div> <!-- div que fecha o container -->
+    </div>
+    <div class="carousel-item">
+      <div class="container">
+
+        <h2 class="mt-4 mb-3">
+          <small>Nome da Planta</small>
+        </h2>
+
+        <!-- Área da Imagem e descrição -->
+        <div class="row">
+
+          <div class="col-md-8 text-center">
+            <img class="img-fluid image-border plant-image" src="images/jiboia.jpeg" alt="">
+          </div>
+
+          <div class="col-md-4">
+            <h3 class="my-3">Nome da Planta</h3>
+            <p>Uma descrição da planta aqui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+              euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum
+              nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+            <ul>
+              <li>Espécie</li>
+              <li>Desde dd/mm/aa</li>
+              <li>4 flores</li>
+              <li>Algo do tipo</li>
+            </ul>
+          </div>
+
+        </div>
+        <!-- Fim da área da imagem  -->
+
+        <!-- Linha do tempo da planta -->
+        <h3 class="my-4">Linha do Tempo da Planta</h3>
+
+        <div class="row">
+          <div class="container">
+            <div class="timeline">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+              <img class="img-pq" src="images/jiboia.jpeg" alt="">
+            </div>
+          </div>
+        </div>
+        <!-- Fim da linha do tempo -->
+
+      </div> <!-- div que fecha o container -->
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselPlantas" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselPlantas" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 <?php require_once("footer.php") ?>
