bf86ba59561c66b96d14a87ac7498c2063771633
Yolanda <ymoretto@gmail.com>
2020-03-14 14:44:48 -0300

    update do perfil de usuario

--- perfil-usuario-yo.php
+++ perfil-usuario-yo.php
@@ -10,18 +10,74 @@
             <div class="col-md-9 basic-info-text">
                 <h2 class="nome-usuario">Botânia Plantícia</h2>
                 <hr class="linha-user">
-                <ul>
-                    <li class="detail">Member Since 2020</li>
-                    <li class="detail">Plants: 1</li>
-                    <li>About me: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam ad, fugit quasi odit provident eaque voluptas ea numquam totam laudantium? Voluptate cumque eaque eveniet temporibus similique ducimus ex asperiores accusantium!</li>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item detail">Member Since 2020</li>
+                    <li class="list-group-item detail">Plants: 1</li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam ad, fugit quasi odit provident eaque voluptas ea numquam totam laudantium? Voluptate cumque eaque eveniet temporibus similique ducimus ex asperiores accusantium!
+                        <br><span class="badge badge-primary badge-pill">editar</span>
+                    </li>
+                
                 </ul>
-                <div>
-                    <p>Badges:</p>
-                    <img class="badge" src="images/badges/051-ecologism-23.png" alt="primeira planta">
+                <div class="badges-session">
+                    <strong>Badges:</strong>
+                    <span>
+                        <img class="accomplishment" src="images/badges/051-ecologism-23.png" alt="primeira planta">
+                    </span> <!-- if this code is not finished, it's because coronavirus got me before I could finished it-->
+                    <span>
+                        <img class="accomplishment" src="images/badges/051-ecologism-9.png" alt="primeira planta">
+                    </span> 
+                    <span>
+                        <img class="accomplishment" src="images/badges/051-ecologism-18.png" alt="primeira planta">
+                    </span>
                 </div>
-            </div>
 
         </div> <!-- close row -->
+
+    </div> <!-- close container -->
+</section>
+
+<section id="plantas-usuario">
+    <div class="container">
+        <div class="row">
+            <h2 class="titulo h2-perfil">Plantas de Plantícia</h2>
+        </div>
+
+        <div class="row">
+
+            <div class="col">
+
+                <div class="timeline">
+                    <img class="img-pq" src="images/jiboia.jpeg" alt="">
+                    <img class="img-pq" src="images/jiboia.jpeg" alt="">
+                    <img class="img-pq" src="images/jiboia.jpeg" alt="">
+                </div>
+
+            </div> <!-- close col -->
+        </div> <!-- close row-->
+
+    </div> <!-- close container -->
+</section>
+
+
+<section id="amigos-usuario">
+    <div class="container">
+        <div class="row">
+            <h2 class="titulo h2-perfil">Amigos de Plantícia</h2>
+        </div>
+
+        <div class="row">
+
+            <div class="col">
+
+                <div class="timeline">
+                    <img class="img-pq friend-image" src="images/helo.jpeg" alt="">
+                    <img class="img-pq friend-image" src="images/helo.jpeg" alt="">
+                    <img class="img-pq friend-image" src="images/helo.jpeg" alt="">
+                </div>
+
+            </div> <!-- close col -->
+        </div> <!-- close row-->
 
     </div> <!-- close container -->
 </section>
