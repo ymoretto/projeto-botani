72e317c7c958195acd0c42d3bbae6bc83ddb5ca9
Yolanda <ymoretto@gmail.com>
2020-03-14 12:06:48 -0300

    update perfil-planta

--- perfil-planta.php
+++ perfil-planta.php
@@ -1,66 +1,52 @@
 <?php require_once("menu-usuario.php") ?>
 
-<div class="container">
+<section id="info-planta">
+    <div class="container">
+        <div class="row">
+            <h2 class="mt-4 mb-3"> Cacilda </h2>
+        </div>
 
-<h2 class="mt-4 mb-3">Plantas do Fulano
-  <small>Nome da Planta</small>
-</h2>
+        <div class="row">
+            <div class="col text-center"> <!-- div da imagem de perfil da planta -->
+                <img class="img-fluid image-border plant-image" src="images/jiboia.jpeg" alt="Imagem da Cacilda"> <!-- tirei img-fluid -->
+                <!-- a imagem está configurada para mudar de tamanho com media query -->
+                <!-- seria legal pensar em uma forma de automatizar isso -->
+            </div>
+            <div class="col plant-description"> <!-- div das informações da planta -->
+                <h3 class="my-3">Nome da Planta</h3>
+                <p>Uma descrição da planta aqui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+                <ul>
+                  <li>Espécie</li>
+                  <li>Desde dd/mm/aa</li>
+                  <li>4 flores</li>
+                  <li>Algo do tipo</li>
+                </ul>
+            </div> <!-- close plant col -->
+        </div> <!-- close row -->
 
-<!-- Área da Imagem e descrição -->
-<div class="row">
+    </div> <!-- close container -->
 
-  <div class="col-md-8 text-center">
-    <img class="img-fluid image-border plant-image" src="images/jiboia.jpeg" alt="">
-  </div>
+    <div class="space"></div> <!--Serve apenas para criar um espaço entre os blocos-->
 
-  <div class="col-md-4">
-    <h3 class="my-3">Nome da Planta</h3>
-    <p>Uma descrição da planta aqui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-    <ul>
-      <li>Espécie</li>
-      <li>Desde dd/mm/aa</li>
-      <li>4 flores</li>
-      <li>Algo do tipo</li>
-    </ul>
-  </div>
+    <div class="container">
+        <div class="row timeline-row">
+            <h2>
+                Timeline da Cacilda
+            </h2>
+            <div class="timeline">
+                <img class="img-pq" src="images/jiboia.jpeg" alt="">
+                <img class="img-pq" src="images/jiboia.jpeg" alt="">
+                <img class="img-pq" src="images/jiboia.jpeg" alt="">
+                <img class="img-pq" src="images/jiboia.jpeg" alt="">
+                <img class="img-pq" src="images/jiboia.jpeg" alt="">
+                <img class="img-pq" src="images/jiboia.jpeg" alt="">
+                <img class="img-pq" src="images/jiboia.jpeg" alt="">
+            </div>
+        </div>
+    </div>
 
-</div>
-<!-- Fim da área da imagem  -->
 
-<!-- Linha do tempo da planta -->
-<h3 class="my-4">Linha do Tempo da Planta</h3>
-
-<div class="row">
-
-  <div class="col-md-3 col-sm-6 mb-4">
-    <a href="#">
-      <img class="img-fluid img-pq" src="images/jiboia.jpeg" alt="">
-    </a>
-  </div>
-
-  <div class="col-md-3 col-sm-6 mb-4">
-    <a href="#">
-      <img class="img-fluid img-pq" src="images/jiboia.jpeg" alt="">
-    </a>
-  </div>
-
-  <div class="col-md-3 col-sm-6 mb-4">
-    <a href="#">
-      <img class="img-fluid img-pq" src="images/jiboia.jpeg" alt="">
-    </a>
-  </div>
-
-  <div class="col-md-3 col-sm-6 mb-4">
-    <a href="#">
-      <img class="img-fluid img-pq" src="images/jiboia.jpeg" alt="">
-    </a>
-  </div>
-
-</div>
-<!-- Fim da linha do tempo -->
-
-</div> <!-- div que fecha o container -->
-
+</section>
 
 
 
