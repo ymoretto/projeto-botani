29213149d3e461d8f25275cdda7b7e202c7de7a9
Yolanda <ymoretto@gmail.com>
2020-03-14 13:27:36 -0300

    teste de imagem

---
+++ perfil-usuario-yo.php
@@ -0,0 +1,30 @@
+<?php require_once("menu-usuario.php") ?>
+
+<section id="info-usuario">
+    <div class="container">
+        <div class="row mt-3">
+            <div class="col mt-5">
+                <img class="img-user" src="images/perfil-botania-planticia.jpg" alt="Foto de Perfil">
+            </div>
+
+            <div class="col-md-9 basic-info-text">
+                <h2 class="nome-usuario">Botânia Plantícia</h2>
+                <hr class="linha-user">
+                <ul>
+                    <li class="detail">Member Since 2020</li>
+                    <li class="detail">Plants: 1</li>
+                    <li>About me: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam ad, fugit quasi odit provident eaque voluptas ea numquam totam laudantium? Voluptate cumque eaque eveniet temporibus similique ducimus ex asperiores accusantium!</li>
+                </ul>
+                <div>
+                    <p>Badges:</p>
+                    <img class="badge" src="images/badges/051-ecologism-23.png" alt="primeira planta">
+                </div>
+            </div>
+
+        </div> <!-- close row -->
+
+    </div> <!-- close container -->
+</section>
+
+
+<?php require_once("footer.php") ?>
