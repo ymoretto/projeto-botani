4ae7e8829b5c663aa88ea07917081bcc1a4e6b52
Heloísa Medeiros <heloisam.silva@hotmail.com>
2020-03-17 18:03:20 -0300

    pagina de cadastro de planta responsiva

--- cadastro-planta.php
+++ cadastro-planta.php
@@ -1,12 +1,12 @@
 <?php require_once("menu-usuario.php") ?>
 <section>
-    <article>
+    <article class="holepage">
         <h2 class="titulo tituloPlanta">Nova planta</h2>
     </article>
     <article class="cadastroPlanta">
         <form action="" enctype="multipart/form-data" method="post" class="formulario">
-            <div class="form-row no-gutters">
-                <div class="col-md-4">
+            <div class="form-row">
+                <div class="col-lg-4">
                     <div class="form-group">
                         <div class="custom-file">
                             <input type="file" class="custom-file-input" id="imagemPlanta" name="imagemPlanta">
@@ -17,26 +17,30 @@
                         <img class="fotoNovaPlanta" src="images/jiboia.jpeg" alt="Foto da nova planta">
                     </div>
                 </div>
-                <div class="col-md-8">
-                    <div class="form-group">
-                        <label for="apelidoPlanta" class="labels">Apelido</label>
-                        <input type="text" name="apelidoPlanta" id="apelidoPlanta" class="form-control"
-                            placeholder="Gertrudes">
+                <div class="col-lg-8">
+                    <div class="dadosPlanta">
+                        <div class="form-group">
+                            <label for="apelidoPlanta" class="labels">Apelido</label>
+                            <input type="text" name="apelidoPlanta" id="apelidoPlanta" class="form-control"
+                                placeholder="Gertrudes">
+                        </div>
+                        <div class="form-group">
+                            <label for="sobrePlanta" class="labels">Sobre</label>
+                            <textarea class="form-control" name="sobrePlanta" id="sobrePlanta"
+                                rows="5">Gertrudes foi um presente dos meus pais para minha nova casa! Mas meu gato quer comer ela.....</textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="especiePlanta" class="labels">Espécie</label>
+                            <select class="form-control" name="especiePlanta" id="especiePlanta">
+                                <option>Orquídea</option>
+                                <option>Samambaia</option>
+                                <option>Rosa</option>
+                            </select>
+                        </div>
+                        <div class="btnPlaceholder">
+                            <button type="submit" class="btn btnCadastro">Cadastrar Planta</button>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="sobrePlanta" class="labels">Sobre</label>
-                        <textarea class="form-control" name="sobrePlanta" id="sobrePlanta"
-                            rows="5">Gertrudes foi um presente dos meus pais para minha nova casa! Mas meu gato quer comer ela.....</textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="especiePlanta" class="labels">Espécie</label>
-                        <select class="form-control" name="especiePlanta" id="especiePlanta">
-                            <option>Orquídea</option>
-                            <option>Samambaia</option>
-                            <option>Rosa</option>
-                        </select>
-                    </div>
-                    <button type="submit" class="btn btnCadastro">Cadastrar Planta</button>
                 </div>
             </div>
         </form>
