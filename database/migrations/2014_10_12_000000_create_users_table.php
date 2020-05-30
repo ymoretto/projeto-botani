eb0ddf84c114c530c7059c0715234cb5404795df
Heloísa Medeiros <heloisam.silva@hotmail.com>
2020-05-30 19:00:34 -0300

    migrations iniciadas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username', 50)->unique();
+            $table->string('name', 100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
