9404a066521ed27851563d35da325c0dd1dba968
Yolanda <ymoretto@gmail.com>
2020-06-02 12:29:18 -0300

    auth com banco de dados

---
+++ database/migrations/2020_06_02_144618_create_plants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plants', function (Blueprint $table) {
+            $table->id();
+            $table->text('description')->nullable();
+            $table->string('especie');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plants');
+    }
+}
