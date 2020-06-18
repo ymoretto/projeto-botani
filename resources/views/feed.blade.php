b8867091cfb48e7d39c6d986682d74c20eb107fd
Yolanda <ymoretto@gmail.com>
2020-06-17 22:41:41 -0300

    feed quase pronto

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -42,43 +42,53 @@
         </div>
         <!-- FINAL DE UM POST -->
 
-        <?php for ($i=0; $i <= 3; $i++) { 
-            echo 
-            "<div class='post'>
-            <div class='row no-gutters'>
-                <div class='col-lg-6 postPhotoPlaceholder'>
-                    <img src='images/jiboia.jpeg'>
+        
+        <!-- COMEÇO DO FOREACH -->
+
+        <?php
+            use App\Post;
+            $posts = Post::all();
+            ?>
+
+        @foreach($posts as $p)
+            
+        <div class="post">
+            <div class="row no-gutters">
+                <div class="col-lg-6 postPhotoPlaceholder">
+                    <img src="/storage/{{ $p->plant->image }}">
                 </div>
-                <div class='col-lg-6 dadosPostPlaceholder'>
-                    <div class='dadosUsuarioPostPlaceholder'>
-                        <img class='userPostIcon' src='images/perfil-botania-planticia.jpg'>
-                        <p><b>botania_planticia</b></p>
+                <div class="col-lg-6 dadosPostPlaceholder">
+                    <div class="dadosUsuarioPostPlaceholder">
+                        <img class="userPostIcon" src="images/perfil-botania-planticia.jpg">
+                        <a href="perfil-usuario.php"><p><b>{{ $p->plant->user->name}}</b></p></a>
                     </div>
-                    <div class='postDescriptionPlaceholder'>
-                        <p><b>Gertrudes - Samambaia</b></p>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id eum quae hic. Beatae illo
-                            architecto optio! Ab, autem adipisci voluptatibus animi perferendis inventore alias cumque,
-                            provident deserunt, hic porro architecto.</p>
-                        <small>21 de março de 2020</small>
+                    <div class="postDescriptionPlaceholder">
+                        <a href="perfil-planta.php"><p><b>{{ $p->plant->name}}</b></p></a>
+                        <p>{{ $p->description }}</p>
+                        <a href="post.php"><small>{{ date( 'd/m/Y' , strtotime($p->created_at))}}</small></a>
                     </div>
-                    <div class='postActionsPlaceholder'>
-                        <img class='postActionIcon' src='images/hearticon.svg'>
-                        <button data-toggle='modal' data-target='#newCommentModal'>
-                            <img class='postActionIcon' src='images/commenticon.svg'>
+                    <div class="postActionsPlaceholder">
+                        <img class="postActionIcon" src="images/hearticon.svg">
+                        <button data-toggle="modal" data-target="#newCommentModal">
+                            <img class="postActionIcon" src="images/commenticon.svg">
                         </button>
                     </div>
-                    <div class='postCommentsPlaceholder'>
+                    <div class="postCommentsPlaceholder">
                         <p><b>Comentários</b></p>
-                        <div class='postComment'>
+                        <div class="postComment">
                             <p><b>helomdrs</b></p>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>";
-        } ?>
-    </div>
+        </div>
+
+        @endforeach
+
+
+
+
 </section>
 
 <!-- MODAL DE FAZER COMENTÁRIO -->
